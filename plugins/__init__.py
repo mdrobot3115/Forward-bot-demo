0ac8a839f70f43b162053bb466cebb20930bfadd
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-07-15 17:12:26 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -44,10 +44,10 @@ async def help_msg(bot: ace , m: Message):
 
 @ace.on_message(
     filters.chat(AUTH_USERS) & filters.private &
-    filters.incoming & filters.command("restart", prefixes=prefixes)
+    filters.incoming & filters.command("cancel", prefixes=prefixes)
 )
 async def restart_handler(_, m):
-    await m.reply_text("Restarted!", True)
+    await m.reply_text("Forwarding stopped!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 @ace.on_message(
