2731301c4c0d76b725d6b1c3743129f8b1ad02a1
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-22 05:59:02 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,31 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+
 from pyrogram import filters
-from pyrogram import Client 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram import Client as ace
+from pyrogram.types import Message
 from main import LOGGER, prefixes, AUTH_USERS
 from config import Config
+import time
 import os
-import sys
 
-@Client.on_message(filters.command('start') & filters.user(AUTH_USERS))
-async def start(client,message):
-        insert(int(message.chat.id))
-        await message.reply_text(text =f"""
-        Hello {message.from_user.first_name }
-        __I'm an advanced forward bot. click /help__
-        """,reply_to_message_id = message.message_id ,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/VysakhXD"),
-                 InlineKeyboardButton("⚡️ Updates", url="https://t.me/VysakhXD")],
-                [InlineKeyboardButton("Help", callback_data="help")],
-            ],
-        )
-        ) 
-                 
-@Client.on_message(filters.command('help') & filters.user(AUTH_USER))
-async def help(client,message): 
-        insert(int(message.chat.id))
-        await message.reply_text(text =f"""
-        __I can Forward message from one chat to another\navailable Commands are\n /forward to start forwarding__
-        """
-        
+
+@ace.on_message(
+    filters.chat(AUTH_USERS) & filters.private &
+    filters.incoming & filters.command("ace", prefixes=prefixes)
+)
+async def forward(bot: ace , m: Message):
+    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
+    t_chat = msg.forward_from_chat.id
+    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
+    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**")
+   # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id)
+    i_chat = msg1.forward_from_chat.id
+    s_msg = int(msg1.forward_from_message_id)
+    f_msg = int(msg2.forward_from_message_id)+1
+    await m.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file')
+    try:
+        for i in range(s_msg, f_msg):
+            try:
+                await bot.copy_message(
+                    chat_id= t_chat,
+                    from_chat_id= i_chat,
+                    message_id= i
+                )
+                time.sleep(2)
+            except Exception:
+                continue
+    except Exception as e:
+        await m.reply_text(str(e))
+    await m.reply_text("Done Forwarding")
