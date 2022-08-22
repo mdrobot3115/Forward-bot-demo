672cf482b52277d5eea20c73e39005c63030cb96
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-22 06:01:23 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,42 +1,58 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
-
 from pyrogram import filters
 from pyrogram import Client as ace
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from main import LOGGER, prefixes, AUTH_USERS
 from config import Config
-import time
 import os
+import sys
 
 
 @ace.on_message(
     filters.chat(AUTH_USERS) & filters.private &
-    filters.incoming & filters.command("ace", prefixes=prefixes)
+    filters.incoming & filters.command("start", prefixes=prefixes)
 )
-async def forward(bot: ace , m: Message):
-    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
-    t_chat = msg.forward_from_chat.id
-    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
-    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**")
-   # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id)
-    i_chat = msg1.forward_from_chat.id
-    s_msg = int(msg1.forward_from_message_id)
-    f_msg = int(msg2.forward_from_message_id)+1
-    await m.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file')
-    try:
-        for i in range(s_msg, f_msg):
-            try:
-                await bot.copy_message(
-                    chat_id= t_chat,
-                    from_chat_id= i_chat,
-                    message_id= i
-                )
-                time.sleep(2)
-            except Exception:
-                continue
-    except Exception as e:
-        await m.reply_text(str(e))
-    await m.reply_text("Done Forwarding")
+async def Start_msg(bot: ace , m: Message):
+    await bot.send_photo(
+    m.chat.id,
+    photo="https://telegra.ph/file/d77a3767a8d58da76f2df.jpg",
+    caption = f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n" +
+    f"\nI am Auto Forwarder bot." +
+    f"\nPress /help for More Info.\n\n__**Developer** : ACE\n**Language** : Python\n**Framwork** : Pyrogram__",
+    # parse_mode="md",
+    reply_markup=InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🙋‍♂️Dev Ace", url="https://t.me/AceCallRobot")],
+            [InlineKeyboardButton("Channel", url="https://t.me/WickedSkull")],
+            [InlineKeyboardButton("Repo", url="https://github.com/imacekun/ACE-AUTO-FORWARD/")],
+        ],
+    )
+    )
+
+
+@ace.on_message(
+    filters.chat(AUTH_USERS) & filters.private &
+    filters.incoming & filters.command("help", prefixes=prefixes)
+)
+async def help_msg(bot: ace , m: Message):   
+    await bot.send_message(
+        m.chat.id,
+        f"**!/usr/bin/env python \n(c) ACE**" +
+        f"\n\nI can Forward message from one chat to another\n"+
+        f"Available Commands are :"+
+        f"\n\n/ace to start forwarding\n/log - To get Log file\n/restart - To Restart the bot"
+    )
+
+@ace.on_message(
+    filters.chat(AUTH_USERS) & filters.private &
+    filters.incoming & filters.command("restart", prefixes=prefixes)
+)
+async def restart_handler(_, m):
+    await m.reply_text("Restarted!", True)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+@ace.on_message(
+    filters.chat(AUTH_USERS) & filters.private &
+    filters.incoming & filters.command("log", prefixes=prefixes)
+)
+async def log_msg(bot: ace , m: Message):   
+    await bot.send_document(m.chat.id, "log.txt")
