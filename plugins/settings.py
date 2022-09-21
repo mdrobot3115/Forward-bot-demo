f7897f9037b53d044fc659c0a069ce46a9ef058c
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-21 17:33:26 +0530

    Create settings.py

---
+++ plugins/settings.py
@@ -0,0 +1,48 @@
+import asyncio
+from translation import Translation
+from pyrogram import Client, filters
+from .test import CLIENT
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+ 
+@Client.on_message(filters.command('settings'))
+async def settings(client, message):
+   await message.reply_text(
+     "<b>change your settings as your wish</b>",
+     reply_markup=main_buttons()
+     )
+    
+@Client.on_callback_query(filters.regex(r'^settings'))
+async def settings_query(bot, query):
+  user_id = query.from_user.id
+  i, type = query.data.split("#")
+  buttons = [[InlineKeyboardButton('back', callback_data="settings#main")]]
+  
+  if type=="main":
+     await query.message.edit_text(
+       "<b>change your settings as your wish</b>",
+       reply_markup=main_buttons())
+       
+  elif type=="bots":
+     buttons = [] 
+     _bot = await db.get_bot(user_id)
+     if _bot is not None:
+        buttons.append([InlineKeyboardButton(_bot['name'],
+                         callback_data=f"settings#editbot")])
+     else:
+        buttons.append([InlineKeyboardButton('✚ Add bot ✚', 
+                         callback_data="settings#addbot")])
+        buttons.append([InlineKeyboardButton('✚ Add User bot ✚', 
+                         callback_data="settings#adduserbot")])
+     buttons.append([InlineKeyboardButton('back', 
+                      callback_data="settings#main")])
+     await query.message.edit_text(
+       "<b><u>My Bots</b></u>\n\n<b>You can manage your bots in here</b>",
+       reply_markup=InlineKeyboardMarkup(buttons))
+  
+  elif type=="addbot":
+     await query.message.delete()
+     bot = await CLIENT.add_bot(bot, query)
+     if bot != True: return
+     await query.message.reply_text(
+        "<b>bot token successfully added to db</b>",
+        reply_markup=InlineKeyboardMarkup(buttons)) 
