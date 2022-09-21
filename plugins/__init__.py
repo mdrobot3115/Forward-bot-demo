dbc3966a790e8c0ea1aa9d06ba03ac4a911e9c94
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-21 17:24:50 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,6 +8,27 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
 from main import LOGGER, prefixes, AUTH_USERS
 
+main_buttons = [[
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/venombotupdates'),
+        InlineKeyboardButton('📢 Update Channel ', url='https://t.me/venombotsupport')
+        ],[
+        InlineKeyboardButton('❗️Help', callback_data='help') 
+        ],[
+        
+]]
+
+#===================Start Function===================#
+
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+    user = message.from_user
+    reply_markup = InlineKeyboardMarkup(main_buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.START_TXT.format(
+                message.from_user.first_name))
+
 @ace.on_message(
     filters.chat(AUTH_USERS) & filters.private &
     filters.incoming & filters.command("cancel", prefixes=prefixes)
