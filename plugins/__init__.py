3d5d85feb3d4c76aa15be4e899be11c348afd43b
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-21 17:54:09 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -95,5 +95,5 @@ async def restart_handler(_, m):
     filters.chat(AUTH_USERS) & filters.private &
     filters.incoming & filters.command("log", prefixes=prefixes)
 )
-async def log_msg(bot: ace , m: Message):   
+async def log_msg(bot: Client , m: Message):   
     await bot.send_document(m.chat.id, "log.txt")
