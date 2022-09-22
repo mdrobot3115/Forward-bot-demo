9e179e06d9a86be40b3164079cbd0ee6f8e56cde
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-22 06:41:06 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -98,5 +98,47 @@ async def restart_handler(_, m):
     filters.incoming & filters.command("log", prefixes=prefixes)
 )
 async def log_msg(client, message):
-    await client.send_document(message.chat.id, "log.txt")
+    await client.send_document(message.chat.id, "log.txt") 
 
+@Client.on_message(filters.command('settings'))
+async def settings(client, message):
+   await message.reply_text(
+     "<b>change your settings as your wish</b>",
+     reply_markup=main_buttons()
+     )
+    
+@Client.on_callback_query(filters.regex(r'^settings'))
+async def settings_query(bot, query):
+  user_id = query.from_user.id
+  i, type = query.data.split("#")
+  buttons = [[InlineKeyboardButton('back', callback_data="settings#main")]]
+  
+  if type=="main":
+     await query.message.edit_text(
+       "<b>change your settings as your wish</b>",
+       reply_markup=main_buttons())
+       
+  elif type=="bots":
+     buttons = [] 
+     _bot = await db.get_bot(user_id)
+     if _bot is not None:
+        buttons.append([InlineKeyboardButton(_bot['name'],
+                         callback_data=f"settings#editbot")])
+     else:
+        buttons.append([InlineKeyboardButton('✚ Add bot ✚', 
+                         callback_data="settings#addbot")])
+        buttons.append([InlineKeyboardButton('✚ Add User bot ✚', 
+                         callback_data="settings#adduserbot")])
+     buttons.append([InlineKeyboardButton('back', 
+                      callback_data="settings#main")])
+     await query.message.edit_text(
+       "<b><u>My Bots</b></u>\n\n<b>You can manage your bots in here</b>",
+       reply_markup=InlineKeyboardMarkup(buttons))
+  
+  elif type=="addbot":
+     await query.message.delete()
+     bot = await CLIENT.add_bot(bot, query)
+     if bot != True: return
+     await query.message.reply_text(
+        "<b>bot token successfully added to db</b>",
+        reply_markup=InlineKeyboardMarkup(buttons)) 
