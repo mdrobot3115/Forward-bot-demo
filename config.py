b1b9ee8b2ea15ceafda8452e6ee746d0ea2df3ba
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-22 08:12:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
 
-import os
+from os import environ 
 
-class Config(object):
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    # The Telegram API things
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("OWNER")
-
+class Config:
+    API_ID = int(environ.get("API_ID", 12345))
+    API_HASH = environ.get("API_HASH")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = environ.get("DATABASE", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", 1411070838).split()]
+    
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+    
