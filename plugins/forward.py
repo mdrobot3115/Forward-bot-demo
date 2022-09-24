bb5b2e59896e0ffeedeb5eea648b04b1245ffa05
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-24 19:51:45 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -17,11 +17,11 @@ import math
 @Client.on_message(filters.command('forward') & filters.user(AUTH_USERS))
 async def forward(bot, message):
     msg = await bot.ask(message.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
-    t_chat = msg.forward_from_chat_id
+    t_chat = msg.forward_from_chat
     msg1 = await bot.ask(message.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
     msg2 = await bot.ask(message.chat.id, "**Send Ending Message from same chat**")
    # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id)
-    i_chat = msg1.forward_from_chat_id
+    i_chat = msg1.forward_from_chat
     s_msg = int(msg1.forward_from_message_id)
     f_msg = int(msg2.forward_from_message_id)+1
     await message.reply_text("Your Current settings are:\n\n➥ From Chat:")
