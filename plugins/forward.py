e8fa0f3d975979bf9d3dcccbd2703a56b70a5f99
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-24 11:48:50 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -33,7 +33,5 @@ async def allow(bot, message):
                 chat_id= t_chat,
                 from_chat_id= i_chat,
                 message_id= i 
-               )
-   except Exception as e:
-     await message.reply_text(str(e))
-     await message.reply_text("🥳 Task Successfully Completed! 🥳")
+            ) 
+            await bot.send_messge("🔰 FORWARDING STATUS 🔰\n
