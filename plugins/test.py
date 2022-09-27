18c5127ac5a76b0df351de008b2217bda83702f9
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-27 07:14:04 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,37 +1,3 @@
-from pyrogram import raw
-from pyrogram import types 
-from pyrogram.errors import UserMigrate
-from pyrogram.session import Session, Auth
-class SignInBot:
+@Client.on_message((filters.forwarded & filters.private & filters.incoming)
     async def sign_in_bot( 
-        while True:
-            try:
-                r = await self.invoke(
-                    raw.functions.auth.ImportBotAuthorization(
-                        flags=0,
-                        api_id=self.api_id,
-                        api_hash=self.api_hash,
-                        bot_auth_token=bot_token
-                    )
-                )
-            except UserMigrate as e:
-                await self.session.stop()
-
-                await self.storage.dc_id(e.value)
-                await self.storage.auth_key(
-                    await Auth(
-                        self, await self.storage.dc_id(),
-                        await self.storage.test_mode()
-                    ).create()
-                )
-                self.session = Session(
-                    self, await self.storage.dc_id(),
-                    await self.storage.auth_key(), await self.storage.test_mode()
-                )
- 
-                await self.session.start()
-            else:
-                await self.storage.user_id(r.user.id)
-                await self.storage.is_bot(True)
-
-                return types.User._parse(self, r.user)
+        
